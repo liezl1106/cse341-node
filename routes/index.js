64021f4b4b7c1ad5509b10294ce4a0783ced8606
liezl76 <131637313+liezl76@users.noreply.github.com>
2025-01-10 14:05:42 +0800

    edit index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const jokes = require('../controllers/');
 
-routes.get('/', jokes.displayJoke);
+//routes.get('/', jokes.displayJoke);
+
+routes.get('/', (req, res) => { res.send('Hello World');});
 
 module.exports = routes;

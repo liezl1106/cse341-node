9ab2e8010c961a6a2708e40a11452267e7eabf2a
liezl76 <lzel_76@yahoo.com>
2025-01-12 18:36:00 +0800

    edit files cse341

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-//const jokes = require('../controllers/');
-
-//routes.get('/', jokes.displayJoke);
-
-routes.get('/', (req, res) => { res.send('Hello World');});
-
-module.exports = routes;

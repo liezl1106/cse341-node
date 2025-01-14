5e6f31d9bfa5abad17c99e8feb9fb02755ab71fc
liezl76 <lzel_76@yahoo.com>
2025-01-15 02:03:25 +0800

    Add code in controllers and edit routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,12 +1,35 @@
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
 
 const getData = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    //res.status(200).json(lists[0]); // we just need the first one (the only one)
-    res.status(200).json(lists); // all list
+    res.status(200).json(lists[1]); // we just need the first one (the only one)
+    //res.status(200).json(lists); // all list
   });
 };
 
-module.exports = { getData };
+const getAll = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('contacts').find();
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+  });
+};
+
+const getSingle = async (req, res, next) => {
+  const contactsId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('contacts').find({_id: contactsId});
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts[0]);
+  });
+};
+
+module.exports = { 
+  getData,
+  getAll,
+  getSingle 
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const contactsController = require('../controllers/contacts');
-
 const router = express.Router();
 
 // GET /feed/posts
 router.get('/', contactsController.getData);
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
 // localhost:3000/contacts/
 module.exports = router;
